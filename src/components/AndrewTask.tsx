3a2c1395fbfde64b2dd37d1c897d1824a23731ba
dorothy <dorothy.x.ren@gmail.com>
2024-06-04 16:41:36 -0400

    new msg

--- src/components/AndrewTask.tsx
+++ src/components/AndrewTask.tsx
@@ -8,7 +8,7 @@ export const AndrewTask = () => {
     const checkedBoxStyles = checked ? 'bg-green-500 border border-green-500' : 'bg-white border border-gray-200'
 
     return (
-        <div className={`${checkedStyles} checkbox-container`}>
+        <div className={`${checkedStyles} flex flex-row max-w-sm w-[90%] h-16 items-center p-4 rounded-lg`}>
             <button className={`w-6 h-6 ${checkedBoxStyles} rounded-md`} onClick={() => setChecked(!checked)}></button>
             <div className="flex-col text-md ml-4">
                 <div className="text-green-900 font-bold">Title</div>
